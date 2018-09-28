0fc7fb41ce111b6cf627287ae6e53ccbd6be1ddb
Scott Russell <scottedwardrussell@gmail.com>
2018-09-28 16:37:35 -0400

    Updated resize-image to use magick package

--- tools/resize-images.R
+++ tools/resize-images.R
@@ -1,31 +1,21 @@
 # Script to resize image files
-# install.packages('remotes')
-# remotes::install_bioc('EBImage')
+# install.packages('magick')
 #
-# if that doesn't work (linux and/or not yet released changes):
-# install.packages('devtools')
-#
-# fails on no openssl
-# sudo apt install -y libssl-dev
-# devtools::install_github('r-lib/remotes')
-#
-# fails on fftw3.h
-# sudo apt install -y libfftw3-3 libfftw3-dev libtiff5-dev 
-#
-# fails on rcurl
-# sudo apt install -y libcurl4-openssl-dev
-# remotes::install_bioc('EBImage')
+# Convert individual images to JPG in order to reference in both local development and deployed app
+# library(magick)
+# image <- image_read("image-name.png")
+# image_write("image, path = "image-name.jpg", format = "jpg")
 
-library("EBImage")
-library("tools")
+library(magick)
+library(tools)
 
 resize_image <- function(image_path, image_filename, dest_dir = "resized", height_dim = 150) {
   # load image
-  original_image <- readImage(file.path(image_path, image_filename))
+  original_image <- image_read(file.path(image_path, image_filename))
 
   # can either scale to specific size, or by relative amount
   # i want to scale the height to 150 pixels, with aspect ratio maintained
-  new_image <- resize(original_image, h = 150)
+  new_image <- image_scale(original_image, paste("x", height_dim, sep = ""))
 
   # write resized image to same directory
   new_image_filename <- file.path(
@@ -40,12 +30,12 @@ resize_image <- function(image_path, image_filename, dest_dir = "resized", heigh
       sep = "_")
     )
   dir.create(file.path(image_path, dest_dir), showWarnings = FALSE)
-  writeImage(new_image, new_image_filename)
+  image_write(new_image, path = new_image_filename, format = "jpg")
 }
 
 image_path <- "notes/images"
 images <- list.files(image_path)
 
-for(image in list_files_with_exts(image_path, c('jpg', 'jpeg', 'png'), full.names = FALSE)) {
+for(image in list_files_with_exts(image_path, "jpg", full.names = FALSE)) {
   resize_image(image_path, image)
 }
